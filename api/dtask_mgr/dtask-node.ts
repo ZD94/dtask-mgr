d204a4dff1265bd51937de9cc9e0aa18f0c53f85
wanglihui <lihui.wang@tulingdao.com>
2017-06-24 15:24:58 +0800

    add 增加node 刷新自己函数

--- api/dtask_mgr/dtask-node.ts
+++ api/dtask_mgr/dtask-node.ts
@@ -1,8 +1,13 @@
 
 import { DTaskDesc, RunTaskParams } from './dtask-desc';
 
+export interface PingResponse {
+    pongAt: number;
+}
+
 export interface INodeHandle{
     runTask(params: RunTaskParams, cb: (err: any, ret: Object)=>void): void;
+    ping(params: any):PingResponse;
 }
 
 export class DTaskNode{
@@ -11,17 +16,32 @@ export class DTaskNode{
     current_task_count = 0;
     handle: INodeHandle|null;
     online: boolean;
+    refreshAt?: number;
+    private refreshTimer: any;
+
     constructor(public id: string, options: {ip: string, concurrency?: number}){
         this.ip = options.ip;
         this.concurrency = options.concurrency || 0;
         this.handle = null;
         this.online = false;
     }
+
     onconnected(handle: INodeHandle){
         this.handle = handle;
         this.online = true;
+        this.refreshAt = Date.now();
+        this.refreshTimer = setInterval( ()=> {
+            if (!this.refreshAt || this.refreshAt + 60 * 1000 < Date.now()) {
+                console.warn(`node-`, this.id,`可能已经掉线`);
+                this.ondisconnected();
+            }
+        }, 30 * 1000);
     }
+    
     ondisconnected(){
+        if (this.refreshTimer) {
+            clearInterval(this.refreshTimer);
+        }
         this.online = false;
         this.handle = null;
     }
