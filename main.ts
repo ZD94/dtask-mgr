5e7e0d7303a8af7488479dc35db8a9dabfd1345d
Ke Peng <ke.peng@jingli365.com>
2017-06-28 14:15:02 +0800

    使用forever-monitor监控程序并重启

--- main.ts
+++ main.ts
@@ -8,6 +8,8 @@ let logger = new Logger('dtask');
 
 import * as zone from '@jingli/zone-setup';
 
+import * as forever from 'forever-monitor';
+
 process.on('unhandledRejection', (reason: any, p: PromiseLike<any>) => {
     if (config.debug) {
         throw reason;
@@ -15,11 +17,21 @@ process.on('unhandledRejection', (reason: any, p: PromiseLike<any>) => {
     logger.error(reason);
 });
 
-zone.forkStackTrace()
-    .run(async function(){
-        await API.initSql(path.join(__dirname, 'api'), config.api);
-        await API.init(path.join(__dirname, 'api'), config.api);
-        await API.startServices(config.listen);
-        logger.info('API initialized.');
+if (process.argv[2] == '-d') {
+    let child = new (forever.Monitor)(process.argv[1], { args: process.argv.slice(3), });
+    child.on('start', function() {
+        logger.info('Forever starting child...');
     });
-
+    child.on('restart', function() {
+        logger.error('Forever restarting child for', child['times'], 'time');
+    });
+    child.start();
+} else {
+    zone.forkStackTrace()
+        .run(async function(){
+            await API.initSql(path.join(__dirname, 'api'), config.api);
+            await API.init(path.join(__dirname, 'api'), config.api);
+            await API.startServices(config.listen);
+            logger.info('API initialized.');
+        });
+}
